ceaaed5b37f8040060484b4c36b8eaa9e8b1ee61
ngarukaalx <hiramkabuagi@gmail.com>
2023-08-09 19:31:18 +0000

    0to99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for num in range(0, 100):
     if num < 10:
         print("0{}, ".format(num), end="")
     else:
-        print("{}, ".format(num), end="" if num < 99 else "\n")
+        print("{}".format(num), end=", " if num < 99 else "\n")
