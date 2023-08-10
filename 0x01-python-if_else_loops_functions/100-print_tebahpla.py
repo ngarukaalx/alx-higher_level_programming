1b9c3837a00ea82ff3da27193ac54f25a446977b
ngarukaalx <hiramkabuagi@gmail.com>
2023-08-10 00:42:39 +0000

    last

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for num1 in range(122, 96, -2):
-    for num2 in range(89, 65, -2):
-        print("{}{}".format(chr(num1), chr(num2)), end="")
-
+dat = 0
+for i in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(i - dat)), end="")
+    dat = 32 if dat == 0 else 0
