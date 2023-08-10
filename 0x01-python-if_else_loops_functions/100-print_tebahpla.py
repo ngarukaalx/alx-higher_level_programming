4af52263ecc28c55d08b32cdf971e7fb986e76a5
ngarukaalx <hiramkabuagi@gmail.com>
2023-08-10 00:10:23 +0000

    dn

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for num1 in range(122, 96 - 2):
-    for num2 in range(89, 65 - 2):
+for num1 in range(122, 96, -2):
+    for num2 in range(89, 65, -2):
         print("{}{}".format(chr(num1), chr(num2)), end="")
+
