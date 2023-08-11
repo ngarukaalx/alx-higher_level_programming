c7362b22b60f5a63d8397713934780c85b0c461b
ngarukaalx <hiramkabuagi@gmail.com>
2023-08-11 16:58:19 +0000

    3questions

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-lengt = len(sys.argv[1:])
-if lengt == 0:
-    print("{} arguments.".format(lengt))
-elif lengt == 1:
-    print("{} argument:".format(lengt))
-    for i, ar in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, ar))
-else:
-    print("{} arguments:".format(lengt))
-    for index, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(index, arg))
+if __name__ == "__main__":
+    import sys
+    lengt = len(sys.argv[1:])
+    if lengt == 0:
+        print("{} arguments.".format(lengt))
+    elif lengt == 1:
+        print("{} argument:".format(lengt))
+        for i, ar in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(i, ar))
+    else:
+        print("{} arguments:".format(lengt))
+        for index, arg in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(index, arg))
