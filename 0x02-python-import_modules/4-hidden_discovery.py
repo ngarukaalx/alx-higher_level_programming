d99c3bde4b16433a887e5fb01da92cafd76727df
ngarukaalx <hiramkabuagi@gmail.com>
2023-08-11 19:11:32 +0000

    dir

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
+    import hidden_4
     string = dir(hidden_4)
     sorted_str = sorted(string)
     for i in sorted_str:
-        print("{}".format(i))
+        if i[:2] != "__":
+            print("{}".format(i))
