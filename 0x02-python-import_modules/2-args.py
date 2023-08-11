e4f9fb77d24e1e017e0eca4a3ae5c1ca0f9da438
ngarukaalx <hiramkabuagi@gmail.com>
2023-08-11 16:37:56 +0000

    sys

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+lengt = len(sys.argv[1:])
+if lengt == 0:
+    print("{} arguments.".format(lengt))
+elif lengt == 1:
+    print("{} argument:".format(lengt))
+    for i, ar in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, ar))
+else:
+    print("{} arguments:".format(lengt))
+    for index, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(index, arg))
