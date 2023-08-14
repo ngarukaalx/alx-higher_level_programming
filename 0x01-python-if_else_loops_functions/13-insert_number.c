7a32cd3a8576d7c784243eed917fc770a646930b
ngarukaalx <hiramkabuagi@gmail.com>
2023-08-14 16:15:38 +0000

    addnode

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * createNode - parameter
+ * @n: parameter
+ *
+ * Return: newnode
+ */
+
+listint_t* createNode(int n)
+{
+        listint_t* newNode = (listint_t*)malloc(sizeof(listint_t));
+        if (newNode != NULL)
+        {
+                newNode->n = n;
+                newNode->next = NULL;
+        }
+        return newNode;
+}
+
+/**
  * insert_node - fuction name
  * @**head: parameter
  * @number: parameter
