a504c9af76970a969fe32331e15388d99f1f2b8b
ngarukaalx <hiramkabuagi@gmail.com>
2023-08-29 16:59:47 +0000

    quiz1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class Square:
+    """A class to represent a square. """
+    def __init__(self, size):
+        """nstantiation with size"""
+        self.__size = size
