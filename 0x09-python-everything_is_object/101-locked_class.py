1cc1a82877b0165dd4815374de67298af8aad8da
ngarukaalx <hiramkabuagi@gmail.com>
2023-09-07 13:20:02 +0000

    magician

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a class that 
+prevents the user from dynamically creating new instance attributes
+"""
+
+
+class LockedClass:
+    """ class LockedClass"""
+    __slots__ = ('first_name',)
