54e5bd4a0fbbadbe82b2917de99ce9bf4d5c0fdb
ngarukaalx <hiramkabuagi@gmail.com>
2023-10-19 18:19:15 +0000

    dumps

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+--  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY title ASC, genre_id ASC;
