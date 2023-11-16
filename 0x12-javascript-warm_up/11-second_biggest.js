3e7ebf1f568238371b93007d7afdfd90f49ad0f8
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-16 09:12:55 +0000

    sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2);
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
-  const newData = args.sort();
-  const secondLasgest = newData[newData.length - 2];
+  const newData = args.map(Number).sort((a, b) => b - a);
+  const secondLasgest = newData[1];
   console.log(secondLasgest);
 }
