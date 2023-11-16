a3732af328aa5c86ea3bd14d05cf6ae0f4d4fa31
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-16 01:39:20 +0000

    semistandard

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,60 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.size; i++) {
+        let row = '';
+        for (let j = 0; j < this.size; j++) {
+          row += 'C';
+        }
+        console.log(row);
+      }
+    } else {
+      for (let s = 0; s < this.size; s++) {
+        let outPut = '';
+        for (let v = 0; v < this.size; v++) {
+          outPut += 'X';
+        }
+        console.log(outPut);
+      }
+    }
+  }
+}
+
+module.exports = Square;
