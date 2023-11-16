1a63a1ecde4acfffe247809bce4f4b73bb9c5e7e
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-16 16:00:34 +0000

    mandatory

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-function factorial(a) {
+function factorial (a) {
   if (a === 0) {
     return 1;
   } else {
-      return a * factorial(a - 1);
+    return a * factorial(a - 1);
   }
 }
 
