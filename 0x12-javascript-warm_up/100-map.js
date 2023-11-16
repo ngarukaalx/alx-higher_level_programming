93e0ad617a2806d284f305187cb83efd705a4a77
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-16 20:19:04 +0000

    updaterepo

--- 0x12-javascript-warm_up/100-map.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const list = require('./100-data').list;
-const newList = list.map(function (element, index) {
-  return element * index;
-});
-console.log(list);
-console.log(newList);
