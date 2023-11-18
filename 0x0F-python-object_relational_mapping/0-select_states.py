d40988f00bcf7e3a312be4906cec4747611dd74d
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-18 11:38:16 +0000

    fixrequirement

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
     """not to be executed when imported"""
-    import MySQLdb
-    import sys
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
     connection = MySQLdb.connect(
-                host='127.0.0.1',
+                host="localhost",
                 port=3306,
-                user=username,
-                passwd=password,
-                db=database
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3]
                 )
 
     cursor = connection.cursor()
-    sq_query = ("""SELECT *
-            FROM states""")
-    cursor.execute(sq_query)
+    cursor.execute("SELECT * FROM states")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
