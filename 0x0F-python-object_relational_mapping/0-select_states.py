7a21ae327d986923bfc86183621e0c4943ad78b1
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-18 10:23:04 +0000

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,10 +2,12 @@
 
 """script that lists all states from the database hbtn_0e_0_usa"""
 
-import MySQLdb
-import sys
 
 if __name__ == "__main__":
+    """not to be executed when imported"""
+    import MySQLdb
+    import sys
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
