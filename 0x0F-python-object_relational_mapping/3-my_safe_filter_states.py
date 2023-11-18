98018cffdef3954f1b6634b6948fa05ab7e9780c
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-18 15:37:23 +0000

    injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """not to be executed when imported"""
+
+    connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cursor = connection.cursor()
+    spl_query = """
+            SELECT * FROM states
+            WHERE name LIKE BINARY %s
+            """
+
+    cursor.execute(spl_query, (sys.argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connection.close()
