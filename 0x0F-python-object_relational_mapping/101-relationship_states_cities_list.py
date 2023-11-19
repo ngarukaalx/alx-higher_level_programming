dae26470f9a2c00a2995d2a9b8b0a1066bad0751
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-19 23:50:26 +0000

    fixs

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     username, password, db_name = sys.argv[1:4]
 
     engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
             .format(username, password, db_name), pool_pre_ping=True
             )
 
